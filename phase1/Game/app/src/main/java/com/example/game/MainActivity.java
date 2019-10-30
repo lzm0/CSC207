14ebac33321986b45bd1689498fab6aee47c1651
Zimo Li <zm.li@mail.utoronto.ca>
2019-10-29 21:12:14 -0400

    Added login dialogue

--- phase1/Game/app/src/main/java/com/example/game/MainActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.game;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Build.VERSION_CODES;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +18,8 @@ import android.view.WindowManager;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+  private String username = "Username";
 
   @RequiresApi(api = VERSION_CODES.KITKAT)
   @Override
@@ -31,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Display main activity
     setContentView(R.layout.activity_main);
+
+    // Disable the play button
+    findViewById(R.id.play_button).setEnabled(false);
   }
 
   public void startGame(View view) {
@@ -38,8 +48,22 @@ public class MainActivity extends AppCompatActivity {
     startActivity(intent);
   }
 
-  public void settings(View view) {
-    Intent intent = new Intent(this, SettingsActivity.class);
-    startActivity(intent);
+  public void login(View view) {
+    // Display a dialogue
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle("Enter your username:");
+    final EditText input = new EditText(this);
+    input.setInputType(InputType.TYPE_CLASS_TEXT);
+    input.setText("Username");
+    builder.setView(input);
+    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        // Set username and enable play button
+        username = input.getText().toString();
+        findViewById(R.id.play_button).setEnabled(true);
+      }
+    });
+    builder.show();
   }
 }
