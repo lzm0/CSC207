fe403020cafbe308b513a51dfb723376f37c4544
Zimo Li <zm.li@mail.utoronto.ca>
2019-10-31 20:16:43 -0400

    Reformat UserManager

--- phase1/Game/app/src/main/java/com/example/game/UserManager.java
+++ phase1/Game/app/src/main/java/com/example/game/UserManager.java
@@ -1,52 +1,36 @@
 package com.example.game;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 
-public class UserManager {
-    private final String File_Name = "GameData";
-    private int level;
-    private String username;
+class UserManager {
+
+  private final String File_Name = "GameData";
+  private String username;
 
 
-    public UserManager(String username) {
-        this.username = username;
-    }
+  UserManager(String username) {
+    this.username = username;
+  }
 
 
-    public void setUserInfo(Context context, int number) {
-        this.level = number;
-        SharedPreferences userInfo = context.getSharedPreferences(File_Name, Context.MODE_PRIVATE);
-        SharedPreferences.OnSharedPreferenceChangeListener changeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+  void setUserInfo(Context context, int number) {
+    SharedPreferences userInfo = context.getSharedPreferences(File_Name, Context.MODE_PRIVATE);
+    SharedPreferences.OnSharedPreferenceChangeListener changeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
 
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-            }
-        };
-        userInfo.registerOnSharedPreferenceChangeListener(changeListener);
-        SharedPreferences.Editor editor = userInfo.edit();
-        editor.putInt(username + "Level", level);
-        editor.apply();
-    }
+      @Override
+      public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+      }
+    };
+    userInfo.registerOnSharedPreferenceChangeListener(changeListener);
+    SharedPreferences.Editor editor = userInfo.edit();
+    editor.putInt(username + "Level", number);
+    editor.apply();
+  }
 
-    public int getUserInfo(Context context) {
-        SharedPreferences userInfo = context.getSharedPreferences(File_Name, Context.MODE_PRIVATE);
-        return userInfo.getInt(username + "Level", 1);
-    }
-
-    public void removeUserInfo(Context context, String key) {
-        SharedPreferences userInfo = context.getSharedPreferences(File_Name, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = userInfo.edit();
-        editor.remove(key + "Level");
-        editor.apply();
-    }
-
-    public void clearUserInfo(Context context) {
-        SharedPreferences userInfo = context.getSharedPreferences(File_Name, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = userInfo.edit();
-        editor.clear();
-        editor.apply();
-    }
+  int getUserInfo(Context context) {
+    SharedPreferences userInfo = context.getSharedPreferences(File_Name, Context.MODE_PRIVATE);
+    return userInfo.getInt(username + "Level", 1);
+  }
 }
 
